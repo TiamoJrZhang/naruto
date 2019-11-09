06c201800efc11fb98585a38adfc58f2b12c0214
jack <94135345@qq.com>
2019-11-09 21:19:18 +0800

    添加eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,32 @@
 module.exports = {
-  // 指定解析器
-  'parse': '',
-  // 指定解析器选项
-  'parserOptions': {
-    "ecmaFeatures": {
-      "jsx": true
+  parser: '@typescript-eslint/parser', //定义ESLint的解析器
+  extends: [
+    'prettier/@typescript-eslint', //使得@typescript-eslint中的样式规范失效，遵循prettier中的样式规范
+    'plugin:prettier/recommended' //使用prettier中的样式规范，且如果使得ESLint会检测prettier的格式问题，同样将格式问题以error的形式抛出
+  ], //定义文件继承的子规范
+  plugins: [
+    'react',
+    '@typescript-eslint'
+  ], //定义了该eslint文件所依赖的插件
+  env: { //指定代码的运行环境
+    browser: true,
+    node: true,
+  },
+  settings: { //自动发现React的版本，从而进行规范react代码
+    "react": {
+      "pragma": "React",
+      "version": "detect"
+    }
+  }, 
+  parserOptions: { //指定ESLint可以解析JSX语法
+    "ecmaVersion": 2019,
+    "sourceType": 'module',
+    "ecmaFeatures":{
+      jsx: true
     }
   },
-  // 指定脚本的运行环境
-  'env': {},
-  // 别人可以直接使用你配置好的ESLint
-  'root': true,
-  // 脚本在执行期间访问的额外的全局变量
-  'globals': {},
-  // 启用的规则及其各自的错误级别
-  'rules': {
-    "prettier/prettier": "error"
+  rules: {
+    'no-unused-vars': 1,
+    'no-useless-escape': 1
   }
 }
---
+++ app/app.tsx
@@ -0,0 +1,12 @@
+/** @format */
+
+import * as React from 'react'
+
+class App extends React.Component {
+  render() {
+    var a = 1
+    return <div>{a}</div>
+  }
+}
+
+export default App
