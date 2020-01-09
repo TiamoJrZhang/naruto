38b64c18e121e72a368590be928037018f05c5f8
jack <94135345@qq.com>
2020-01-10 03:35:12 +0800

    addloopclone

--- src/resource/deepclone.js
+++ src/resource/deepclone.js
@@ -2,13 +2,14 @@
 /**
  * 深度拷贝代码实现
  * 解决循环引用，采用hash表的方式
- * 解决symbol对象拷贝，使用Reflect.ownKeys
- * 
+ * 解决symbol属性值拷贝，使用Reflect.ownKeys
+ * 方式一采用递归的方式，但是会爆栈
+ * 方式二采用循环的方式，破解递归爆栈
  */
 function isObject(source) {
   return typeof source === 'object' && source !== null
 }
-
+// 方式一
 function deepClone(source, hash = new Map()) {
   if (!isObject(source)) return source
   if (hash.get(source)) return hash.get(source)
@@ -24,12 +25,42 @@ function deepClone(source, hash = new Map()) {
   return target
 }
 
+//方式二
+function cloneLoop(source) {
+  const res = {}
+  const stack = [{
+    data: source
+  }]
+
+  while(stack.length) {
+    const node = stack.pop()
+    const data = node.data
+
+    Reflect.ownKeys(data).forEach(v => {
+      if (isObject(data[v])) {
+        stack.push({
+          data: data[v]
+        })
+      } else {
+        res[v] = data[v]
+      }
+    })
+  }
+
+  return res
+
+}
+
 var a = {
 	name: "muyiy",
 	a1: undefined,
 	a2: null,
-	a3: 123
+  a3: 123,
+  data: {
+    time: +new Date()  
+  }
 }
 a.circleRef = a
 
 deepClone(a)
+cloneLoop(a)
