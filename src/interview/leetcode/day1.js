3f21f049e0c098041ab673765e9a4fb3e5e843eb
jack <94135345@qq.com>
2020-06-15 19:07:37 +0800

    s

--- src/interview/leetcode/day1.js
+++ src/interview/leetcode/day1.js
@@ -89,17 +89,64 @@ var addTwoNumbers = function(l1, l2) {
   return node.next
 }
 
-// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
-// 示例 1:
-// 输入: "abcabcbb"
-// 输出: 3
-// 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
-// 示例 2:
-// 输入: "bbbbb"
-// 输出: 1
-// 解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
-// 示例 3:
-// 输入: "pwwkew"
-// 输出: 3
-// 解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
-// 请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+//5. 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+// 示例 1：
+// 输入: "babad"
+// 输出: "bab"
+// 注意: "aba" 也是一个有效答案。
+// 示例 2：
+// 输入: "cbbd"
+// 输出: "bb"
+
+//暴力法
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var longestPalindrome = function(s) {
+  if (s.length < 2) return s
+  var maxlen = 1
+  var left = 0
+  for (var i = 0; i < s.length - 1; i++) {
+    for (var j = i + 1; j < s.length; j++) {
+      if (isPalindrome(s, i, j) && j - i + 1 > maxlen) {
+        left = i
+        maxlen = j - i + 1
+      }
+    }
+  }
+  return s.substring(left, left + maxlen)
+}
+
+function isPalindrome(s, left, right) {
+  while (left < right) {
+    if (s[left] != s[right]) return false
+    left++
+    right--
+  }
+  return true
+}
+
+var longestPalindrome1 = function(s) {
+  var len = s.length
+  if (len < 2) return s
+  var dp = Array.from(new Array(len), () => new Array(len).fill(0))
+  for (var i = 0; i < s.length; i++) {
+    dp[i][i] = true
+  }
+  for (var j = 0; j < len; j++) {
+    for (var i = 0; i < j; i++) {
+      if (s[i] != s[j]) {
+        dp[i][j] = false
+      } else {
+        if (j - i < 3) {
+          dp[i][j] = true
+        } else {
+          dp[i][j] = dp[i + 1][j - 1]
+        }
+      }
+
+      // if (dp[i][j] && )
+    }
+  }
+}
