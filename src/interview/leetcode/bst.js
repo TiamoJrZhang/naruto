435d08a0974817d1d9f182b3c03b9c09197b601e
Tiamo <94135345@qq.com>
2020-08-08 20:55:13 +0800

    bst

--- src/interview/leetcode/bst.js
+++ src/interview/leetcode/bst.js
@@ -17,19 +17,17 @@
  * @return {boolean}
  */
 
-var isValidBST = function (root) {
+var isValidBST = function(root) {
   function helper(node, upper, lower) {
-    if (node == null) 
-      return true
-    if (node.val > upper || node.val < lower) 
-      return false
+    if (node == null) return true
+    if (node.val > upper || node.val < lower) return false
     return helper(node.left, node.val, lower) && helper(node.right, upper, node.val)
   }
 
   helper(root, Infinity, -Infinity)
 }
 
-var isValidBST1 = function (root) {
+var isValidBST1 = function(root) {
   let stack = []
   let inorder = -Infinity
 
@@ -40,8 +38,7 @@ var isValidBST1 = function (root) {
     }
 
     root = stack.pop()
-    if (root.val <= inorder) 
-      return false
+    if (root.val <= inorder) return false
     inorder = root.val
     root = root.right
   }
@@ -49,14 +46,13 @@ var isValidBST1 = function (root) {
   return true
 }
 
-var isValidBST2 = function (root) {
+var isValidBST2 = function(root) {
   let pre = -Infinity
   var result = true
   function helper(node) {
     if (node) {
       result = helper(node.left)
-      if (node.val <= pre) 
-        return (result = false)
+      if (node.val <= pre) return (result = false)
       pre = node.val
       result = helper(node.right)
     }
@@ -68,35 +64,28 @@ var isValidBST2 = function (root) {
 
 //二叉树模板
 function traverse(root) {
-  if (root == null) 
-    return
+  if (root == null) return
   root += 1
   traverse(root.left)
   traverse(root.right)
 }
 
 function isSameTree(root1, root2) {
-  if (root1 == null && root2 == null) 
-    return true
-  if (root1 == null || root2 == null) 
-    return false
-  if (root1.val != root2.val) 
-    return false
+  if (root1 == null && root2 == null) return true
+  if (root1 == null || root2 == null) return false
+  if (root1.val != root2.val) return false
   return isSameTree(root1.left, root2.left) && isSameTree(root1.right, root2.right)
 }
 
 function isInBst(root, target) {
-  if (root == null) 
-    return false
-  if (root.val == target) 
-    return true
+  if (root == null) return false
+  if (root.val == target) return true
 
   return isInBst(root.left, target) || isInBst(root.right, target)
 }
 
 function isInBst2(root, target) {
-  if (root.val == target) 
-    return true
+  if (root.val == target) return true
   if (root.val < target) {
     return isInBst2(root.right, target)
   }
@@ -106,8 +95,7 @@ function isInBst2(root, target) {
 }
 
 function insertTreeNode(root, val) {
-  if (root == null) 
-    return new TreeNode(val)
+  if (root == null) return new TreeNode(val)
   if (val < root.left) {
     root.left = insertTreeNode(root.left, val)
   }
@@ -153,34 +141,27 @@ function isValidBST(root) {
 }
 
 function helper(root, min, max) {
-  if (!root) 
-    return true
-  if (max != null && root.val >= max.val) 
-    return false
-  if (min != null && root.val <= min.val) 
-    return false
+  if (!root) return true
+  if (max != null && root.val >= max.val) return false
+  if (min != null && root.val <= min.val) return false
   return helper(root.left, min, root) && helper(root.right, root, max)
 }
 
 //https://leetcode-cn.com/problems/merge-two-binary-trees/
-var mergeTrees = function (t1, t2) {
-  if (!t1 && !t2) 
-    return null
-  if (!t1 && t2) 
-    return t1 = t2
-  if (t1 && t2) 
-    t1.val += t2.val
+var mergeTrees = function(t1, t2) {
+  if (!t1 && !t2) return null
+  if (!t1 && t2) return (t1 = t2)
+  if (t1 && t2) t1.val += t2.val
   t1.left = mergeTrees(t1.left, t2 && t2.left)
   t1.right = mergeTrees(t1.right, t2 && t2.right)
   return t1
-};
+}
 
 ///https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/submissions/
-var preorder = function (root) {
+var preorder = function(root) {
   var arr = []
-  var dfs = function (root) {
-    if (!root) 
-      return
+  var dfs = function(root) {
+    if (!root) return
     arr.push(root.val)
     for (var i = 0; i < root.children.length; i++) {
       dfs(root.children[i])
@@ -188,18 +169,16 @@ var preorder = function (root) {
   }
   dfs(root)
   return arr
-};
+}
 
-var preorder = function (root) {
-  if (!root) 
-    return []
+var preorder = function(root) {
+  if (!root) return []
   var stack = [root]
   var arr = []
 
   while (stack.length) {
     var item = stack.pop()
-    if (!item) 
-      return
+    if (!item) return
     arr.push(item.val)
     for (var i = item.children.length - 1; i >= 0; i--) {
       stack.push(item.children[i])
@@ -207,13 +186,12 @@ var preorder = function (root) {
   }
 
   return arr
-};
+}
 
 //https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/
-var maxDepth = function (root) {
+var maxDepth = function(root) {
   var max = 0
-  if (!root) 
-    return 0
+  if (!root) return 0
   if (root.children) {
     for (var i = 0; i < root.children.length; i++) {
       max = Math.max(max, maxDepth(root.children[i]))
@@ -221,32 +199,29 @@ var maxDepth = function (root) {
   }
 
   return max + 1
-};
+}
 
-var maxDepth2 = function (root) {
-  if (!root) 
-    return 0
-  if (!root.children) 
-    return 1
+var maxDepth2 = function(root) {
+  if (!root) return 0
+  if (!root.children) return 1
 
   var stack = [
     {
       node: root,
-      deep: 1
-    }
+      deep: 1,
+    },
   ]
   var depth = 0
   while (stack.length) {
     var item = stack.pop()
-    if (!item) 
-      return
+    if (!item) return
     var node = item.node
     var prevDeep = item.deep
     depth = Math.max(prevDeep, depth)
     for (var i = node.children.length - 1; i >= 0; i--) {
       stack.push({
         node: node.children[i],
-        deep: prevDeep + 1
+        deep: prevDeep + 1,
       })
     }
   }
@@ -254,9 +229,8 @@ var maxDepth2 = function (root) {
 }
 
 //https://leetcode-cn.com/problems/sum-of-left-leaves/submissions/
-var sumOfLeftLeaves = function (root) {
-  if (!root) 
-    return 0
+var sumOfLeftLeaves = function(root) {
+  if (!root) return 0
   var stack = [root]
   var sum = 0
   while (stack.length) {
@@ -273,21 +247,21 @@ var sumOfLeftLeaves = function (root) {
     }
   }
   return sum
-};
+}
 
 var sumOfLeftLeaves2 = function(root) {
   var sum = 0
   var helper = function(node) {
-      if (!node) return 0
-      if (node.left && node.left.left == null && node.left.right == null) {
-          sum += node.left.val
-      }
-      helper(node.left)
-      helper(node.right)
+    if (!node) return 0
+    if (node.left && node.left.left == null && node.left.right == null) {
+      sum += node.left.val
+    }
+    helper(node.left)
+    helper(node.right)
   }
   helper(root)
   return sum
-};
+}
 
 //https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 /**
@@ -307,13 +281,13 @@ var lowestCommonAncestor = function(root, p, q) {
   } else {
     return root
   }
-};
+}
 
 var lowestCommonAncestor = function(root, p, q) {
   var pVal = p.val
   var qVal = q.val
 
-  while(root) {
+  while (root) {
     if (pVal > root.val && qVal > root.val) {
       root = root.right
     } else if (pVal < root.val && qVal < root.val) {
@@ -322,7 +296,7 @@ var lowestCommonAncestor = function(root, p, q) {
       return root
     }
   }
-};
+}
 
 //https://leetcode-cn.com/problems/binary-tree-paths/
 /**
@@ -338,13 +312,13 @@ var binaryTreePaths = function(root) {
       arr.push(path)
       return
     }
-    path += root.val + '->' 
+    path += root.val + '->'
     helper(root.left, path)
     helper(root.right, path)
   }
   helper(root, '')
   return arr
-};
+}
 
 //https://leetcode-cn.com/problems/minimum-depth-of-binary-tree/
 /**
@@ -355,21 +329,21 @@ var minDepth = function(root) {
   var minArr = []
   var helper = function(root, mindep) {
     if (!root) return
-    mindep ++
+    mindep++
     if (!root.left && !root.right) {
-        return  minArr.push(mindep)
+      return minArr.push(mindep)
     }
     helper(root.left, mindep)
     helper(root.right, mindep)
   }
-  root ? helper(root, 0) : minArr = [0]
-  return Math.min(...minArr) 
-};
+  root ? helper(root, 0) : (minArr = [0])
+  return Math.min(...minArr)
+}
 //递归典型求最大最小值模板
 var minDepth = function(root) {
   if (!root) return 0
   if (root.left == null && root.right == null) {
-    return min_depth = 1
+    return (min_depth = 1)
   }
   var min_depth = Number.MAX_VALUE
 
@@ -377,15 +351,15 @@ var minDepth = function(root) {
     min_depth = Math.min(minDepth(root.left), min_depth)
   }
   if (root.right) {
-    min_depth = Math.min(minDepth(root.right), min_depth)  
+    min_depth = Math.min(minDepth(root.right), min_depth)
   }
   return min_depth + 1
-};
+}
 
 //https://leetcode-cn.com/problems/path-sum/submissions/
 var hasPathSum = function(root, sum) {
   if (!root) return false
-  var hasSum 
+  var hasSum
   var helper = function(root, end) {
     if (!root) return false
     if (root.left == null && root.right == null) {
@@ -393,21 +367,19 @@ var hasPathSum = function(root, sum) {
       hasSum = sum == end ? true : false
     }
     end = end + root.val
-    !hasSum &&
-        helper(root.left, end)
-    !hasSum && 
-        helper(root.right, end)
-  } 
+    !hasSum && helper(root.left, end)
+    !hasSum && helper(root.right, end)
+  }
   helper(root, 0)
   return hasSum
-};
+}
 
 var hasPathSum2 = function(root, sum) {
   if (!root) return false
   var stack = [root]
   var isSum = false
   var pre = 0
-  while(stack.length) {
+  while (stack.length) {
     var node = stack.pop()
     if (node.left === null && node.right === null) {
       pre += node.val
@@ -415,7 +387,7 @@ var hasPathSum2 = function(root, sum) {
     }
     pre += node.val
     if (node.left && !isSum) {
-      stack.push(node.left) 
+      stack.push(node.left)
     }
 
     if (node.right && !isSum) {
@@ -423,7 +395,7 @@ var hasPathSum2 = function(root, sum) {
     }
   }
   return isSum
-};
+}
 
 //https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
 var inorderTraversal = function(root) {
@@ -431,8 +403,8 @@ var inorderTraversal = function(root) {
   var cur = root
   var stack = []
   var res = []
-  while(stack.length || cur) {
-    while(cur) {
+  while (stack.length || cur) {
+    while (cur) {
       stack.push(cur)
       cur = cur.left
     }
@@ -441,7 +413,7 @@ var inorderTraversal = function(root) {
     cur = cur.right
   }
   return res
-};
+}
 
 //https://leetcode-cn.com/problems/kth-smallest-element-in-a-bst/
 var kthSmallest = function(root, k) {
@@ -449,8 +421,8 @@ var kthSmallest = function(root, k) {
   var index = 0
   var stack = []
   var cur = root
-  while(cur || stack.length) {
-    while(cur) {
+  while (cur || stack.length) {
+    while (cur) {
       stack.push(cur)
       cur = cur.left
     }
@@ -459,10 +431,9 @@ var kthSmallest = function(root, k) {
       return cur.val
     }
     cur = cur.right
-    index ++
+    index++
   }
-};
-
+}
 
 var kthSmallest2 = function(root, k) {
   var res = []
@@ -474,4 +445,46 @@ var kthSmallest2 = function(root, k) {
   }
   helper(root)
   return res[k - 1]
-};
+}
+
+//https://leetcode-cn.com/problems/binary-tree-right-side-view/
+//bfs
+const rightSideView = function(root) {
+  if (!root) return []
+  var queue = [root]
+  var res = []
+  while (queue.length) {
+    var len = queue.length
+    while (len) {
+      var node = queue.shift()
+      if (node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+      if (len == 1) {
+        res.push(node.val)
+      }
+      len--
+    }
+  }
+  return res
+}
+
+//dfs
+const rightSideView2 = function(root) {
+  var res = []
+  var depth = 0
+  var helper = function(root, depth) {
+    if (!root) return
+    if (depth == res.length) {
+      res.push(root.val)
+    }
+    depth++
+    helper(root.right, depth)
+    helper(root.left, depth)
+  }
+  helper(root, depth)
+  return res
+}
