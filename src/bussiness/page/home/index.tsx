aef303f99762f768d88d69cd0b078de11ef3925f
Tiamo <94135345@qq.com>
2020-08-16 14:43:43 +0800

    s

--- src/bussiness/page/home/index.tsx
+++ src/bussiness/page/home/index.tsx
@@ -1,17 +1,78 @@
-import React, {Component} from 'react'
+import React, {Component, useState, useCallback} from 'react'
 import {fakeNew} from '@/interview/index'
-class Home extends Component<any, any> {
-  constructor(props: any) {
-    super(props)
-    this.state = {
-      show: false,
-    }
+
+// class Child extends Component {
+//   // const [count, setCount] = useState(0)
+//   state = {
+//     count: 0
+//   }
+
+//   handleClick = () => {
+//     this.setState({
+//       count: 0
+//     })
+//   }
+
+//   render() {
+//     console.log('child is running')
+//     return (
+//       <button onClick={this.handleClick}>{this.state.count} click me</button>
+//     )
+//   }
+// }
+
+const Child = ({callback}: {callback: () => void}) => {
+  const [count, setCount] = useState(0)
+  const handleClick = () => {
+    callback()
+  }
+  console.log('child is running')
+  return <button onClick={handleClick}>{count} click me</button>
+}
+
+const Home = () => {
+  const [enable, setEnable] = useState(false)
+
+  const callback = useCallback(() => {
+    console.log(111)
+  }, [])
+
+  const handleClick = () => {
+    setEnable(true)
   }
 
-  render() {
-    const {show} = this.state
-    return <>{show && <span>hello word</span>}</>
-  }
+  return (
+    <>
+      <button onClick={handleClick}>test</button>
+      <Child callback={callback} />
+    </>
+  )
 }
+
+// class Home extends Component<any, any> {
+//   constructor(props: any) {
+//     super(props)
+//     this.state = {
+//       count: 0
+//     }
+//   }
+
+//   render() {
+//     console.log('i am running')
+//     const {count} = this.state
+//     return (
+//       <>
+//         <Child />
+//         <span>{count}</span>
+//         {/* <button onClick={() => this.setState((pre: any) => {
+//           return { ...pre, count: 1 }
+//         })}
+//         >
+//           click
+//         </button> */}
+//       </>
+//     )
+//   }
+// }
 
 export default Home
